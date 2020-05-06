2013981d0c42dc503a7971dc08ec32acbe65fd90
Rafli Andreansyah <rafliandreansyah957@gmail.com>
2020-05-06 19:58:22 +0700

    menambah pengecekan jika panjang ingredient 0

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,12 +4,20 @@ import classes from "./Burger.css";
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 const burger = (props) => {
-  const transformIngredients = Object.keys(props.ingredient).map((igKeys) => {
-    return [...Array(props.ingredient[igKeys])].map((_, i) => {
-      return <BurgerIngredient key={igKeys + i} type={igKeys} />;
-    });
-  });
+  let transformIngredients = Object.keys(props.ingredient)
+    .map((igKeys) => {
+      return [...Array(props.ingredient[igKeys])].map((_, i) => {
+        return <BurgerIngredient key={igKeys + i} type={igKeys} />;
+      });
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
 
+  if (transformIngredients.length === 0) {
+    transformIngredients = <p>Please start adding ingredient!</p>;
+  }
+  console.log(transformIngredients);
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
