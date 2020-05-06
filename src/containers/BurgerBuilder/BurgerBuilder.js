e01c4c76a76f6e0bb791326fc8df7f849b61e157
Rafli Andreansyah <rafliandreansyah957@gmail.com>
2020-05-06 17:08:36 +0700

    Menambah state ingredient untuk menentukan jumlah pada ingredient

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,10 +4,19 @@ import Aux from "../../hoc/Aux";
 import Burger from "../../components/Burger/Burger";
 
 class BurgerBuilder extends Component {
+  state = {
+    ingredients: {
+      salad: 1,
+      bacon: 2,
+      cheese: 3,
+      meat: 1,
+    },
+  };
+
   render() {
     return (
       <Aux>
-        <Burger />
+        <Burger ingredient={this.state.ingredients} />
         <div>Burger Control</div>
       </Aux>
     );
