999bfdaf11ca1f169578c305846684d502397a22
Rafli Andreansyah <rafliandreansyah957@gmail.com>
2020-05-19 16:46:06 +0700

    menambah Global handling error axios

---
+++ src/hoc/errorHandling/errorHandling.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+
+import Modal from "../../components/UI/Modal/Modal";
+import Aux from "../Aux/Aux";
+
+const errorHandling = (WrapperComponent, axios) => {
+  return class extends Component {
+    state = {
+      error: null,
+    };
+
+    componentDidMount() {
+      axios.interceptors.request.use((req) => {
+        this.setState({ error: null });
+        return req;
+      });
+
+      axios.interceptors.response.use(
+        (req) => req,
+        (error) => {
+          this.setState({ error: error });
+        }
+      );
+    }
+
+    errorConfirmHandler = () => {
+      this.setState({ error: null });
+    };
+
+    render() {
+      return (
+        <Aux>
+          <Modal show={this.state.error} clicked={this.errorConfirmHandler}>
+            {this.state.error ? this.state.error.message : null}
+          </Modal>
+          <WrapperComponent {...this.props} />
+        </Aux>
+      );
+    }
+  };
+};
+
+export default errorHandling;
