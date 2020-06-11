03c51c86093ef7a20db054db9faf003f77ef52fa
Rafli Andreansyah <rafliandreansyah957@gmail.com>
2020-06-11 14:46:30 +0700

    mengatur reducer

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,10 +1,38 @@
 import * as actionTypes from "./actions";
 
 const initialState = {
-  ingredients: null,
+  ingredients: {
+    bacon: 0,
+    cheese: 0,
+    meat: 0,
+    salad: 0,
+  },
   totalPrice: 4,
 };
 
-const reducer = (state = initialState, action) => {};
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case actionTypes.ADD_INGREDIENTS:
+      return {
+        // Immutable Update Patterns dengan tidak mengakses secara langsung state dan mengkloning state lama untuk update data state
+        ...state,
+        ingredients: {
+          ...state.ingredients,
+          [action.ingredientName]: state.ingredients[action.ingredientName] + 1,
+        },
+      };
+    case actionTypes.REMOVE_INGREDIENTS:
+      return {
+        // Immutable Update Patterns dengan tidak mengakses secara langsung state dan mengkloning state lama untuk update data state
+        ...state,
+        ingredients: {
+          ...state.ingredients,
+          [action.ingredientName]: state.ingredients[action.ingredientName] - 1,
+        },
+      };
+    default:
+      return state;
+  }
+};
 
 export default reducer;
